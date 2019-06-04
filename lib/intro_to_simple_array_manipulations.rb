419a18047192c649ab4e7bbaf280fdaf89af164b
Justice Marks <her@shedaddy.com>
2019-06-04 15:01:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,46 @@
+def using_push(array, string)
+  
+  array.push(string)
+  
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+  
+end
+
+def using_pop(array)
+  return array.pop()
+  
+end
+
+
+def pop_with_args(array,n)
+  
+  return array.pop(n)
+  
+end
+
+def using_shift(array)
+  return array.shift()
+end
+
+def shift_with_args(array,n)
+  return array.shift(n)
+  
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(3)
+end
+
+def using_uniq(array)
+  array.uniq
+  
+end
+
+def using_flatten(array)
